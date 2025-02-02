b2c7f0f6b0301290ba55251df1bf29febaf3c4e1
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-02-03 00:05:31 +1100

    added routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,12 +3,12 @@ import { Blog } from "./blogModel";
 
 const router: Router = express.Router();
 
-// Add console.log to debug
+// GET - all Blogs
 router.get("/blogs", async (req, res) => {
   try {
     console.log("Fetching blogs..."); // Debug log
     const blogs = await Blog.find();
-    console.log("Found blogs:", blogs); // Debug log
+    console.log("Found blogs"); // Debug log
     res.json(blogs);
   } catch (error) {
     console.error("Error fetching blogs:", error); // Debug log
@@ -16,13 +16,42 @@ router.get("/blogs", async (req, res) => {
   }
 });
 
-// GET /api/blogs/:id - Get a specific blog
+// GET:id - specific blog
 router.get("/blogs/:id", async (req, res) => {
   try {
+    // Finds blog based on the req id and sends it back as a JSON document
     const blog = await Blog.findById(req.params.id);
     res.json(blog);
   } catch (error) {
     res.status(404).json({ error: "Blog not found" });
   }
 });
+
+// DELETE:id
+router.delete("/blogs/:id", async (req, res) => {
+  try {
+    const deleteBlog = await Blog.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Blog deleted successfully" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// POST
+router.post("/blogs", async (req, res) => {
+  try {
+    // Creating a new blog based on the req
+    const newBlog = new Blog({
+      title: req.body.title,
+      description: req.body.description,
+      body: req.body.body,
+    });
+
+    const savedBlog = await newBlog.save(); // saving the new Blog
+    res.status(201).json(savedBlog); // sending back the doc
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 export default router;
