2859ef9e7c9bd03c1a81ba24e97f451e79121b91
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-10 00:25:12 +1100

    fixes to the handleLogout function

--- server/src/controllers/logoutController.ts
+++ server/src/controllers/logoutController.ts
@@ -1,28 +1,32 @@
 import { Request, Response } from "express";
 import { User } from "../models/userModel";
+import { CustomRequest } from "../types/types";
 
-export const handleLogout = async (req: Request, res: Response) => {
-  const cookies = req.cookies;
-
-  if (!cookies.jwt) {
+export const handleLogout = async (req: CustomRequest, res: Response) => {
+  console.log("Logging user out");
+  if (!req.cookies.jwt) {
+    console.log("No Cookie");
     res.status(204).json({ message: "No token provided" });
     return;
   }
+  console.log(req.userID);
+  const user = await User.findById(req.userID);
 
-  const refreshToken = cookies.jwt;
-
-  const user = await User.findOne({ refreshToken: refreshToken });
-
+  if (user) {
+    console.log(user);
+  }
   if (!user) {
     res
-      .sendStatus(204)
-      .clearCookie("jwt", { httpOnly: true, secure: true, sameSite: "lax" });
+      .status(204)
+      .clearCookie("jwt", { httpOnly: true, secure: true, sameSite: "lax" })
+      .json({ message: "User logged out succesfully" });
     return;
   }
 
-  await user.updateOne({ refreshToken: "" });
+  await User.findByIdAndUpdate(user._id, { refreshToken: "" }, { new: true });
+
   res
+    .status(200)
     .clearCookie("jwt", { httpOnly: true, secure: true, sameSite: "lax" })
-    .sendStatus(200);
+    .json({ message: "User logged out succesfully" });
 };
- 
