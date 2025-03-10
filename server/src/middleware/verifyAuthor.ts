3a91ace28b229ee98c8cd37f52503c46e49d79c2
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-10 18:40:07 +1100

    Fixed verifyAuthor component.

--- server/src/middleware/verifyAuthor.ts
+++ server/src/middleware/verifyAuthor.ts
@@ -3,6 +3,7 @@ import { Blog, BlogI } from "../models/blogModel";
 import { User } from "../models/userModel";
 import { blogController } from "../controllers/blogController";
 import { CustomRequest } from "../types/types";
+import mongoose from "mongoose";
 
 const verifyAuthor = async (
   req: CustomRequest,
@@ -10,17 +11,18 @@ const verifyAuthor = async (
   next: NextFunction
 ) => {
   console.log("In verifyAuthor");
-  const userID = req.userID;
   const blog = await Blog.findById(req.params.id);
   const user = await User.findById(req.userID);
-  console.log(user);
 
-  if (blog?.authorUserID !== userID || user?.role !== "admin") {
+  if (String(blog?.authorUserID) !== String(user?._id)) {
+    console.log("Unauthorized user detected");
     res.status(401).json({
       message: "You are unauthorised to proceed with this action",
     });
+    return;
   }
 
+  console.log("Author Confirmed");
   next();
 };
 
