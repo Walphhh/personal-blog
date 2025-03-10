2c56a754a280b8265f6236d519bfcfef9fd65859
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-03-11 01:26:19 +1100

    git ignore and server changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.ORIGIN,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
